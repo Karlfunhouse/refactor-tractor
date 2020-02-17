9b988f26236c8151edfd2f8f102d8af0e93dabce
Trond Makonese <trondation@gmail.com>
2020-02-17 13:38:13 -0700

    Able to fetch all information from the api.

--- src/cookbook.js
+++ src/cookbook.js
@@ -4,14 +4,14 @@ class CookBook {
     this.recipesData = recipesData;
   }
 
-  // findRecipe(searchText) {
-  //   return this.recipes.filter(recipe => {
-  //     return recipe.ingredients.find(ingredient => {
-  //       return (ingredient.name.includes(searchText)) ||
-  //       (recipe.name.includes(searchText))
-  //     });
-  //   })
-  // }
+  findRecipe(searchText) {
+    return this.recipes.filter(recipe => {
+      return recipe.ingredients.find(ingredient => {
+        return (ingredient.name.includes(searchText)) ||
+        (recipe.name.includes(searchText))
+      });
+    })
+  }
 }
 
 export default CookBook;
