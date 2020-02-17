b95a8b181d83f47dd015248bdfb75b6f750f24ce
Trond Makonese <trondation@gmail.com>
2020-02-17 13:25:16 -0700

    :crystal_ball: Create cook book mother class.

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,16 +1,17 @@
-class Cookbook {
-  constructor(data) {
-    this.recipes = data;
+class CookBook {
+  constructor(ingredientsData, recipesData) {
+    this.ingredientsData = ingredientsData;
+    this.recipesData = recipesData;
   }
 
-  findRecipe(searchText) {
-    return this.recipes.filter(recipe => {
-      return recipe.ingredients.find(ingredient => {
-        return (ingredient.name.includes(searchText)) ||
-        (recipe.name.includes(searchText))
-      });
-    })
-  }
+  // findRecipe(searchText) {
+  //   return this.recipes.filter(recipe => {
+  //     return recipe.ingredients.find(ingredient => {
+  //       return (ingredient.name.includes(searchText)) ||
+  //       (recipe.name.includes(searchText))
+  //     });
+  //   })
+  // }
 }
 
-export default Cookbook;
+export default CookBook;
