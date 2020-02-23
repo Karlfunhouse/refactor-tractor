c47effb2c8d96c8b87d2c7d363f0b4bad7f88150
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-23 14:14:06 -0700

    :crystal_ball: Added method to getIngredientNames

--- src/pantry.js
+++ src/pantry.js
@@ -6,6 +6,28 @@ class Pantry extends CookBook {
     this.usersPantry = usersPantry;
   }
 
+  // Determine whether my pantry has enough ingredients to cook a given meal
+  getIngredientNames() {
+    // console.log(this.ingredientsData);
+    //iterate over ingredientsData and iterate over pantry ingredients list and compare ingredient.id to pantry.id and return ingredient.name
+    return this.ingredientsData.reduce((ingredientList, ingredient) => {
+     let matchingIngredients = this.usersPantry.filter(pantryIngredient => {
+        if(pantryIngredient.ingredient === ingredient.id) {
+        ingredientList.push(ingredient.name)
+        }
+      })
+      return ingredientList
+    }, [])
+  };
+
+  // Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
+
+  // Remove the ingredients used for a given meal from my pantry, once that meal has been cooked (only applicable if users have a list of mealsToCook; can be considered a stretch goal)
+
+
+
+
+
 }
 
 export default Pantry;
