561752b1af0c3cc95b5e1abc31c6143f6ae7609f
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-23 13:39:56 -0700

    :crystal_ball: Start building constructor & super to build Pantry class

--- src/pantry.js
+++ src/pantry.js
@@ -1,7 +1,11 @@
-class Pantry {
-  constructor(userIngredients) {
-    this.contents = userIngredients;
+import CookBook from "../src/cookbook"
+
+class Pantry extends CookBook {
+  constructor(ingredientsData, recipesData, usersPantry) {
+    super(ingredientsData, recipesData)
+    this.usersPantry = usersPantry;
   }
+
 }
 
 export default Pantry;
