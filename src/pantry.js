004fae9b95ffdac2ccf84bd00409570c45ea425e
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-25 18:42:39 -0700

    :crystal_ball: Added functionality for checkIngredientSupply method; still doesn't get ingredient name

--- src/pantry.js
+++ src/pantry.js
@@ -4,30 +4,48 @@ class Pantry extends CookBook {
   constructor(ingredientsData, recipesData, usersPantry) {
     super(ingredientsData, recipesData)
     this.usersPantry = usersPantry;
+    // console.log('ingredientsData', ingredientsData);
+    // console.log('recipesData', recipesData);
+    // console.log('usersPantry', usersPantry);
   }
 
   // Determine whether my pantry has enough ingredients to cook a given meal
   getIngredientNames() {
-    // console.log(this.ingredientsData);
-    //iterate over ingredientsData and iterate over pantry ingredients list and compare ingredient.id to pantry.id and return ingredient.name
     return this.ingredientsData.reduce((ingredientList, ingredient) => {
      let matchingIngredients = this.usersPantry.filter(pantryIngredient => {
         if(pantryIngredient.ingredient === ingredient.id) {
-        ingredientList.push(ingredient.name)
+        ingredientList.push({name: ingredient.name, amount: pantryIngredient.amount})
         }
       })
+      // console.log(ingredientList);
       return ingredientList
     }, [])
   };
 
   // Determine the amount of ingredients still needed to cook a given meal, based on what’s in my pantry
 
-  // Remove the ingredients used for a given meal from my pantry, once that meal has been cooked (only applicable if users have a list of mealsToCook; can be considered a stretch goal)
+  checkIngredientSupply() {
+      const recipeIngredients = this.recipesData[0].ingredients;
+      // console.log(recipeIngredients);
+      const pantry = this.usersPantry;
+      // console.log(pantry);
+      return recipeIngredients.map(recipeIngredient => {
+        const thePantryItem = pantry.find(pantryItem => pantryItem.ingredient === recipeIngredient.id)
+    if (!thePantryItem) {
+      console.log(`item not in pantry, go buy ${recipeIngredient.quantity.amount} ${recipeIngredient.quantity.unit}s of ${recipeIngredient.name}`);
+      return `item not in pantry, go buy ${recipeIngredient.quantity.amount} ${recipeIngredient.quantity.unit}s of ${recipeIngredient.name}`;
 
+    } else if (thePantryItem.amount >= recipeIngredient.quantity.amount) {
+      console.log(`Let's get cookin!`);
+      return `Let's get cookin!`;
 
-
-
-
+    } else {
+      let deficit = recipeIngredient.quantity.amount - thePantryItem.amount
+      console.log(`You need ${deficit} more ${recipeIngredient.quantity.unit} of ${recipeIngredient.name}`);
+      return `You need ${deficit} more ${recipeIngredient.quantity.unit} of ${recipeIngredient.name}`
+      }
+    })
+  }
 }
 
 export default Pantry;
