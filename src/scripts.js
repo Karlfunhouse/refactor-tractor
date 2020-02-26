e44b1aa675f631d34941ac9b7c07d43ee2a45089
Madison Randle <madisonrandle25@gmail.com>
2020-02-26 00:00:37 -0700

    Add recipesToCook functionality

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,9 @@
-
 import './css/styles.scss';
 
-import domUpdates from './domUpdates'
+import domUpdates from './domUpdates';
 import CookBook from './cookbook';
-import Pantry from './pantry'
-import Users from './user'
+import Pantry from './pantry';
+import Users from './user';
 import $ from 'jquery';
 
 let currentUser;
@@ -47,14 +46,16 @@ Promise.all([recipeData, ingredientData, userData])
   .then(() => {
     shuffleUser(usersData);
     cookBook = new CookBook(ingredientsData, recipesData, searchedList);
-    currentUser = new Users(usersData[0], favorites, toCook)
-    currentUsersPantry = new Pantry(usersData[0].pantry)
-    onStartUp()
+    currentUser = new Users(usersData[0], favorites, toCook);
+    currentUsersPantry = new Pantry(usersData[0].pantry);
+    onStartUp();
     $('.all-cards').click(cardButtonConditionals);
     $('#home-button').click(homeButtonHandler);
     $('#view-favorites-button').click(viewFavoritesHandler);
+    $('#view-to-cook-button').click(viewToCookHandler);
     $('.favorite').click(favoriteRecipe);
     $('#search-button').click(searchHandler);
+    $('.add').click(cookMe);
     searchValue;
     // $('.unfavorite').click(unFavoriteRecipe);
     $('.to-cook-button').click(cookMe);
@@ -80,8 +81,12 @@ const homeButtonHandler = (event) => {
 
 const viewFavoritesHandler = () => {
   $('.all-cards').empty();
-  domUpdates.populateFavorites(currentUser)
+  domUpdates.populateFavorites(currentUser);
+}
 
+const viewToCookHandler = () => {
+  $('.all-cards').empty();
+    domUpdates.populateToCook(currentUser);
 }
 
 const searchHandler = () => {
@@ -92,7 +97,6 @@ const searchHandler = () => {
 }
 
 const favoriteRecipe = (event) => {
-  console.log('favorite')
   let specificRecipe = cookBook.recipesData.find(recipe => {
     if (recipe.id  === Number(event.target.id)) {
       $(`.${recipe.id}`).addClass('favorite-active');
@@ -103,15 +107,13 @@ const favoriteRecipe = (event) => {
 }
 
 const cookMe = (event) => {
-  console.log('to-cook')
+
   let letscook = cookBook.recipesData.find(recipe => {
-    if (recipe.id  === Number(event.target.id)) {
-      // $(`.${recipe.id}`).addClass('favorite-active');
-      return recipe;
-    }
+  return recipe.id === parseInt(event.target.id)
   })
+
   currentUser.addToCook(letscook);
-  console.log(currentUser)
+
 }
 
 // const toggleFavoriteHelper = (specificRecipe) => {
