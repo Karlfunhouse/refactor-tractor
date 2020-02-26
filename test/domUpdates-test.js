b5774d98ccbadbb8ef46b74801cec7e13b92757f
Madison Randle <madisonrandle25@gmail.com>
2020-02-25 20:29:49 -0700

    Setup and write tests for greetUser method

---
+++ test/domUpdates-test.js
@@ -0,0 +1,44 @@
+import chai from 'chai';
+import $ from 'jquery';
+import domUpdates from '../src/domUpdates';
+
+const expect = chai.expect;
+const spies = require('chai-spies');
+chai.use(spies);
+
+describe.only('domUpdates.js', () => {
+  chai.spy.on(domUpdates, 'greetUser', () => 'User\'s Name');
+  it('Should invoke greetUser', () => {
+    expect(domUpdates.greetUser()).to.equal('User\'s Name');
+  });
+  chai.spy.on(domUpdates, 'populateIngredients', () => 'All ingredients');
+
+});
+
+
+
+// import chai from 'chai';
+// import $ from 'jquery'
+// import Customer from '../src/customer'
+// import Manager from '../src/manager'
+
+// const expect = chai.expect;
+// const spies = require('chai-spies');
+// chai.use(spies);
+
+// let customer = new Customer(2, 'Rocio Schuster', '2020/01/12');
+// let manager = new Manager('manager', '2020/01/13');
+
+
+// describe('customer', () => {
+//   chai.spy.on(customer, ['addAvailableCabinsToBookingSearch'], () => true);
+//   it('should invoke addAvailableCabinsToBookingSearch', () => {
+//     expect(customer.addAvailableCabinsToBookingSearch()).to.equal(true);
+//   });
+//   chai.spy.on(customer, ['checkForFilters'], () => true);
+//   it('should invoke checkForFilters', () => {
+//     expect(customer.checkForFilters()).to.equal(true);
+//   });
+// });
+
+//
